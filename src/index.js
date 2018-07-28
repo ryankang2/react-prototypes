cedcba8195a57c99311c70b8f109678747595ecd
Ryan Kang <shk130@ucsd.edu>
2018-07-28 16:23:29 -0700

    react-intro - Ryan Kang

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const user = {
+    name: "Ryan",
+    luckyNumber: luckyNumber(),
+}
+
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
+
+function luckyNumber(){
+    return Math.floor(Math.random()*1000) + 1;
+}
+
+function greeting(userObj){
+    const container = (<div>
+        <h1>Hello {userObj.name}</h1>
+        <h2 className="text-muted">Your lucky number: {userObj.luckyNumber}</h2>
+    </div>)
+    return container;
+}
+
+
+
