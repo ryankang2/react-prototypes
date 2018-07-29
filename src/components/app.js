6284d2225a071f6f2d64352208b189a3dd577b66
Ryan Kang <shk130@ucsd.edu>
2018-07-28 17:24:46 -0700

    functional-components - Ryan Kang

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,33 @@
+import React from "react";
+import Table from "./table";
 
+function App(){
+    return <h1> My first React functional component </h1>;
+}
 
+export default () => {
+    return (
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+}
+
+const students = [
+    {
+        name: "Phil",
+        course: "CSE1",
+        grade: 99,
+    },
+    {
+        name: "Kevin",
+        course: "Literature",
+        grade: 90,
+    },
+    {
+        name: "Hon",
+        course: "Math",
+        grade: 11,
+    }
+];
